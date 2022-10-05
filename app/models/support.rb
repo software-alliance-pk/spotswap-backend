e8638417e7ee9070dcf19e5d351a31a5dd40b8cf
ror-vs <backend@viralsquare.org>
2022-10-05 14:00:59 +0500

    Add the Create Ticket and Support functionality

---
+++ app/models/support.rb
@@ -0,0 +1,5 @@
+class Support < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :supports, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, uniqueness: true
