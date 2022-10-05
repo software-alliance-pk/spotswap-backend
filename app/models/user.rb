e6e5e921f7391ef7fca1a148f11e978b36391e21
ror-vs <backend@viralsquare.org>
2022-10-05 14:40:36 +0500

    Quick Chat functionality completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :supports, dependent: :destroy
+  has_many :quick_chats, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, uniqueness: true
---
+++ app/views/api/v1/quick_chats/get_all_quick_chat.json.builder
@@ -0,0 +1,4 @@
+json.tickets @quick_chat do |chat|
+  json.id chat.id
+  json.message chat.message
+end
