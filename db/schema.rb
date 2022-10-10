0610acc340967c5ff3c3654a16c2be1172d91aa1
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-10-10 14:59:50 +0500

    add_status_to_supports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_09_072343) do
+ActiveRecord::Schema.define(version: 2022_10_10_095727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 2022_10_09_072343) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_supports_on_user_id"
   end
 
