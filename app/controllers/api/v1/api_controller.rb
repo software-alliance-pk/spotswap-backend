55d7f73e07276715ece6d90c0343ec3d4141f93e
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-10-17 14:35:08 +0500

    update user issue

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -15,7 +15,10 @@ class Api::V1::ApiController < ActionController::API
     header = header.split(' ').last if header
     begin
       @decoded = JsonWebToken.decode(header)
-      @current_user = User.find(@decoded[:user_id])
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+      puts @decoded
+      puts "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+      @current_user = User.find(@decoded[:user_id]) || User.find(@decoded[:email])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
