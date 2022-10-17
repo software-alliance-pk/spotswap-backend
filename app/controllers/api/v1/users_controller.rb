e4bc1d39a95976c5d8bfa64686eb2836ba3b8b0c
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-10-17 14:24:46 +0500

    update user issue

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,22 @@
 class Api::V1::UsersController < Api::V1::ApiController
   before_action :authorize_request
+  before_action :find_user
   before_action :user_params, only: [:update_user]
   
   def get_user
   end
 
   def update_user
-    unless @current_user.update(user_params.merge(profile_complete: true))
+    unless @user.update(user_params.merge(profile_complete: true))
       render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
+  def find_user
+    @user = @current_user
+  end
+
   def user_params
     params.permit(:id, :name, :email, :contact)
   end
