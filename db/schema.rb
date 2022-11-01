6f95d391457f82720b4319b168140bc0e562735d
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-01 17:02:31 +0500

    add_contact_location_status_to_admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_28_171849) do
+ActiveRecord::Schema.define(version: 2022_11_01_113813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,9 @@ ActiveRecord::Schema.define(version: 2022_10_28_171849) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
     t.integer "category", default: 0
+    t.string "contact"
+    t.string "location"
+    t.integer "status", default: 0
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
