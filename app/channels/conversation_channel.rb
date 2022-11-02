38e8084b15980d406e237069c042b9a61e13ee67
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-02 19:27:29 +0500

    conversation channel is completed

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -4,44 +4,11 @@ class ConversationChannel < ApplicationCable::Channel
     if params[:conversation_id].present?
       stream_from("conversation_#{(params[:conversation_id])}")
     else
-      puts "conversation_id is missing."
+      puts "conversation id is missing."
     end
   end
 
   def unsubscribed
     stop_all_streams
   end
-  
-  # calls when a client broadcasts data
-  def speak(data)
-    sender    = get_sender(data)
-    room_id   = data['room_id']
-    message   = data['message']
-
-    raise 'No room_id!' if room_id.blank?
-    convo = get_convo(room_id) # A conversation is a room
-    raise 'No conversation found!' if convo.blank?
-    raise 'No message!' if message.blank?
-
-    # adds the message sender to the conversation if not already included
-    convo.users << sender unless convo.users.include?(sender)
-    # saves the message and its data to the DB
-    # Note: this does not broadcast to the clients yet!
-    Message.create!(
-      conversation: convo,
-      sender: sender,
-      content: message
-    )
-  end
-  
-  # Helpers
-  
-  def get_convo(room_code)
-    Conversation.find_by(room_code: room_code)
-  end
-  
-  def get_sender
-    User.find_by(guid: id)
-  end
-  
 end
