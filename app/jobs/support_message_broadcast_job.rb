8d8803c8eaf86d64d49e63c394174bcdcbd464a6
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-03 19:22:21 +0500

    fixed action cable issue

--- app/jobs/support_message_broadcast_job.rb
+++ app/jobs/support_message_broadcast_job.rb
@@ -3,9 +3,16 @@ class SupportMessageBroadcastJob < ApplicationJob
 
   def perform(message)
     payload = {
+      id: message.id,
+      body: message.body,
       support_conversation_id: message.support_conversation_id,
-      content: message.body,
-      sender: message.user
+      read_status: message.read_status,
+      sender_id: message.sender_id,
+      recepient_id: message.user_id,
+      type: message.type,
+      created_at: message.created_at,
+      message_image: message.image.attached? ? support_message.image.url : "",
+      sender_image: User.find_by_id(message.sender_id).image.attached? ? User.find_by_id(message.sender_id).image.url : ""
     }
     ActionCable.server.broadcast(build_support_conversation_id(message.support_conversation_id), payload)
   end
