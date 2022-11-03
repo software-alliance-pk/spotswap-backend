5b880b53811c6154ad48866f57d805dc72e16a93
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-03 16:16:27 +0500

    modifications in parking slot api

--- app/controllers/api/v1/supports_controller.rb
+++ app/controllers/api/v1/supports_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::SupportsController < Api::V1::ApiController
   before_action :authorize_request
   before_action :support_params, only: [:create_ticket]
+  before_action :find_support_conversation, only: [:create_message, :get_all_support_messages]
 
   def create_ticket
     @ticket = @current_user.supports.new(support_params.merge(ticket_number: generate_ticket_number.upcase))
     if @ticket.save
       @support_conversation = @ticket.build_support_conversation(sender_id: @ticket.user_id, recipient_id: Admin.admin.first.id)
       if @support_conversation.save
-        @support_message = @support_conversation.support_messages.build(user_id: @ticket.user_id, body: @ticket.description, image: support_params[:image])
+        @support_message = @support_conversation.support_messages.build(sender_id: @ticket.user_id, user_id: Admin.admin.first.id, body: @ticket.description, image: support_params[:image])
         @support_message.save
       end
     else
@@ -20,8 +21,13 @@ class Api::V1::SupportsController < Api::V1::ApiController
     @completed_tickets = @current_user.supports.completed.order("created_at desc")
   end
 
+  def get_all_support_messages
+	  @support_messages = @support_conversation.support_messages.order(created_at: :desc)
+  end
+
   def create_message
     @support_message = SupportMessage.new(message_params)
+    @support_message.user_id = Admin.admin.first.id
     if @support_message.save
       @support_message
     else
@@ -38,11 +44,20 @@ class Api::V1::SupportsController < Api::V1::ApiController
 
   private
 
+  def find_support_conversation
+		if params[:support_conversation_id].present?
+			@support_conversation = SupportConversation.find_by(id: params[:support_conversation_id])
+			render json: {error: "Support Conversation with this id is not present."}, status: :unprocessable_entity unless @support_conversation.present?
+		else
+			render json: {error: "Support Conversation id is missing."}, status: :unprocessable_entity
+		end
+	end
+
   def support_params
-    params.permit(:user_id, :ticket_number, :status, :description, :image, :name)
+    params.permit(:status, :description, :image, :name)
   end
 
   def message_params
-    params.permit(:body, :user_id, :support_conversation_id, :image)
+    params.permit(:body, :sender_id, :support_conversation_id, :image)
   end
 end
