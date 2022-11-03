81ee02e6810ea6277845328875b885aba8142642
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-03 18:41:04 +0500

    image required validation false added to support messages response

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,5 @@ class Message < ApplicationRecord
   belongs_to :user
   has_one_attached :image, dependent: :destroy
 
-  validates_presence_of :body
-
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
