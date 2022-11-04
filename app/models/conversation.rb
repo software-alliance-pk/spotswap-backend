98a40961006996fd8ead2b74a17be9496aa82237
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-04 17:07:52 +0500

    get all conversations respnse changed

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,5 @@
 class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
-  #has_and_belongs_to_many :users
+  belongs_to :sender, class_name: "User", foreign_key: :sender_id
+  belongs_to :recepient, class_name: "User", foreign_key: :recepient_id
 end
