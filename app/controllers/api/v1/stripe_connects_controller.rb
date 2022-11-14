be5ad7e3e380ad376894dd7d1eed1128efb3043a
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-14 16:59:10 +0500

    working

--- app/controllers/api/v1/stripe_connects_controller.rb
+++ app/controllers/api/v1/stripe_connects_controller.rb
@@ -1,25 +1,45 @@
 class Api::V1::StripeConnectsController < Api::V1::ApiController
   before_action :authorize_request
 
-  def refresh_stripe_account_link
-    begin
-      @stripe_link = StripeConnectAccountService.new.create_stripe_account_link(@current_user, refresh_stripe_account_link_api_v1_stripe_connects_url)
-      render json: @stripe_link
+  # def refresh_stripe_account_link
+  #   begin
+  #     @stripe_link = StripeConnectAccountService.new.create_stripe_account_link(@current_user, refresh_stripe_account_link_api_v1_stripe_connects_url)
+  #     render json: @stripe_link
       
-    rescue Exception => e
-      render json: { error:  e.message }, status: :unprocessable_entity
-    end
-  end
+  #   rescue Exception => e
+  #     render json: { error:  e.message }, status: :unprocessable_entity
+  #   end
+  # end
 
   def user_stripe_connect_account
     begin
       stripe_connect_account = @current_user.stripe_connect_account
       if stripe_connect_account.present?
-        @account_details = StripeConnectAccountService.new.retrieve_stripe_connect_account_against_given_user(stripe_connect_account.account_id)
+        @account_details = StripeConnectAccountService.new.retrieve_stripe_connect_account(stripe_connect_account.account_id)
       else
         @account_details = StripeConnectAccountService.new.create_connect_customer_account(@current_user, refresh_stripe_account_link_api_v1_stripe_connects_url)
       end
       render json: @account_details
+
+    rescue Exception => e
+      render json: { error:  e.message }, status: :unprocessable_entity
+    end
+  end
+
+  def create_login_link
+    begin
+      link = StripeConnectAccountService.new.create_login_link_of_stripe_connect_account(@current_user.stripe_connect_id)
+      render json: { link: link }
+
+    rescue Exception => e
+      render json: { error:  e.message }, status: :unprocessable_entity
+    end
+  end
+
+  def retrieve_connect_account
+    begin
+      account = StripeConnectAccountService.new.retrieve_stripe_connect_account(@current_user.stripe_connect_id)
+      render json: { account: account }
 
     rescue Exception => e
       render json: { error:  e.message }, status: :unprocessable_entity
