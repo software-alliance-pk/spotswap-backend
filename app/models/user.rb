634deb8de3af702a644ee7978e35155c052cb7e4
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-16 21:52:25 +0500

    updated get all messages api

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ApplicationRecord
   has_many :conversations, dependent: :destroy
   has_one_attached :image, dependent: :destroy
   has_one :parking_slot, dependent: :destroy
-  has_one :swapper_host_connection, dependent: :destroy
+  has_one :swapper_host_connection, dependent: :destroy,class_name: :SwapperHostConnection, foreign_key: :user_id
+  has_one :host_swapper_connection, dependent: :destroy,class_name: :SwapperHostConnection, foreign_key: :host_id
   has_many :user_referral_code_records, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true
