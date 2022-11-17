6b61b6eed3ae20435b31fb91417607808756362a
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-17 13:24:55 +0500

    options response updated in push notification service

--- app/services/push_notification_service.rb
+++ app/services/push_notification_service.rb
@@ -1,10 +1,10 @@
 class PushNotificationService
   require 'fcm'
   def self.fcm_push_notification(connection)
-    data = {connection: connection, body: "Swapper #{connection.swapper.name} and Host #{connection.host.name} has been connected.", host: connection.host, parking_slot: connection.parking_slot, swapper: connection.swapper, finder_car_model: connection.swapper&.car_detail&.car_model}
+    data = {swapper: connection.swapper, host: connection.host, parking_slot: connection.parking_slot, swapper_host_connection_id: connection.id, confirmed_screen: connection.confirmed_screen, car_model_name: connection.swapper&.car_detail&.car_model&.title, user_image: connection.swapper.image.attached? ? connection.swapper.image.url : "", car_image: connection.swapper.car_detail&.photos&.attached? ? connection.swapper.car_detail.photos[0].url : "", parking_slot_image: connection.parking_slot.image.attached? ? connection.parking_slot.image.url : "", body: "Swapper #{connection.swapper.name} and Host #{connection.host.name} has been connected.",}
     fcm_client = FCM.new("AAAAlr4iktw:APA91bF55dfM-lYWPqi-dHMnWGvrwQwRMAEJZD6Hu2P1mEdX8sHBcsVzLx3goF2E8ArNLw9EwvaRzlUGd5YDHCY9WOiu0mtP4jR8XXD2aH-5ItgZ12eY90NYxrNuisHjm3mIx8lsMFAo") # set your FCM_SERVER_KEY
     options = { priority: 'high',
-          data: { data: data, notification_type: "Swapper_Host_Connection" },
+          data: { data: data, notification_type: "connection_created" },
           notification: {
           body: data[:body],
           sound: 'default'
