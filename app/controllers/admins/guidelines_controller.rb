0cd11de000054a1f0a620b486c66e630a605300d
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-21 17:30:34 +0500

    working on guidelines

--- app/controllers/admins/guidelines_controller.rb
+++ app/controllers/admins/guidelines_controller.rb
@@ -2,8 +2,23 @@ class Admins::GuidelinesController < ApplicationController
 	before_action :authenticate_admin!
 
 	def terms_and_conditions
+    @terms_and_conditions = Page.where(title: "Terms & Conditions")
 	end
 
+  def edit_terms_and_conditions
+	end
+
+  def update_terms_and_conditions
+    @page = Page.find_by(id: params[:id])
+    if @page.content.update(body: params[:description])
+      redirect_to terms_and_conditions_admins_guidelines_path
+      flash[:alert] = "Terms and Conditions have been updated successfully."
+    else
+      redirect_to terms_and_conditions_admins_guidelines_path
+      flash[:alert] = @page.content.errors.full_messages.to_sentence
+    end
+	end
+  
   def privacy_policy
 	end
 
