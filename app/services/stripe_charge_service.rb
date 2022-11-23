06eeb8ce6ce236511467b9c9a99d3b3f8098be56
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-23 22:14:00 +0500

    worked on m4 apis

--- app/services/stripe_charge_service.rb
+++ app/services/stripe_charge_service.rb
@@ -1,16 +1,13 @@
 class StripeChargeService
   Stripe.api_key ="sk_test_51LxA5aDG0Cz60XkmJmG5SqF65UOdl7MC8qoJPwfKZdxw09kRSDUnO649B6UhZuzn05DMILFoy4Ptbz8zDSh1NeBy001ulT1oYP"
 
-  def charge_amount_from_customer(amount,source,description)
-    begin
-    Stripe::Charge.create({
-                            amount: 2000,
+  def charge_amount_from_customer(amount)
+    response = Stripe::Charge.create({
+                            amount: amount,
                             currency: 'usd',
                             source: 'tok_visa',
                             description: 'My First Test Charge (created for API docs at https://www.stripe.com/docs/api)',
                           })
-    rescue  Exception => e
-
-    end
+    return response
   end
 end
--- app/services/stripe_top_up_service.rb
+++ app/services/stripe_top_up_service.rb
@@ -2,30 +2,21 @@ class StripeTopUpService
   Stripe.api_key ="sk_test_51LxA5aDG0Cz60XkmJmG5SqF65UOdl7MC8qoJPwfKZdxw09kRSDUnO649B6UhZuzn05DMILFoy4Ptbz8zDSh1NeBy001ulT1oYP"
   Stripe.api_key = ENV['STRIPE_API_KEY'] if Rails.env.production?
 
-  def create_top_up
-    begin
-      #https://stripe.com/docs/api/topups/create?lang=ruby
-      # To add dumpy plan we turn off the pay outs and then call this
-      #  Top-ups are limited to $100,000.00 USD per week. If you need a higher limit, please contact us via https://support.stripe.com/contact.)
-      Stripe::Topup.create(
-        {
-          amount: 2000,
-          currency: 'usd',
-          description: 'Top-up for week of May 31',
-          statement_descriptor: 'Weekly top-up',
-        },
-        )
-    rescue Exception => e
-      #code here
-    end
+  def create_top_up(amount)
+    return Stripe::Topup.create(
+      {
+        amount: amount,
+        currency: 'usd',
+        description: 'Top-up for week of May 31',
+        statement_descriptor: 'Weekly top-up',
+      },
+      )
   end
 
   def retrieve_top_up_info
     #https://stripe.com/docs/api/topups/retrieve
     begin
-    Stripe::Topup.retrieve(
-      'tu_1Lz4qz2eZvKYlo2CnlLtJ2B3',
-      )
+    Stripe::Topup.retrieve('tu_1Lz4qz2eZvKYlo2CnlLtJ2B3')
     rescue Exception => e
     end
   end
