cd1f7d6d3b7587cdf6f87722fcf1c3a99a20c763
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-11-24 14:14:00 +0500

    notify_host_swapper_is_still_interested api added

--- app/services/push_notification_service.rb
+++ app/services/push_notification_service.rb
@@ -110,4 +110,35 @@ class PushNotificationService
      puts "registration id is missing."
    end
   end
+
+  def self.notify_host_swapper_is_still_interested(connection)
+    data = {
+       swapper: connection.swapper,
+       host: connection.host,
+       parking_slot: connection.parking_slot, 
+       swapper_host_connection_id: connection.id,
+       confirmed_screen: connection.confirmed_screen,
+       car_model_name: connection.swapper&.car_detail&.car_model&.title,
+       user_image: connection.swapper.image.attached? ? connection.swapper.image.url : "",
+       car_image: connection.swapper.car_detail&.photos&.attached? ? connection.swapper.car_detail.photos[0].url : "",
+       parking_slot_image: connection.parking_slot.image.attached? ? connection.parking_slot.image.url : "",
+       body: "Swapper #{connection.swapper.name} is still interested in your Parking Slot."
+     }   
+   fcm_client = FCM.new("AAAAlr4iktw:APA91bF55dfM-lYWPqi-dHMnWGvrwQwRMAEJZD6Hu2P1mEdX8sHBcsVzLx3goF2E8ArNLw9EwvaRzlUGd5YDHCY9WOiu0mtP4jR8XXD2aH-5ItgZ12eY90NYxrNuisHjm3mIx8lsMFAo") # set your FCM_SERVER_KEY
+   options = { priority: 'high',
+         data: { data: data, notification_type: "still_interested" },
+         notification: {
+         body: data[:body],
+         sound: 'default'
+         }
+       }
+   registration_id = connection.host&.mobile_device&.mobile_device_token
+   if registration_id.present?
+     response = fcm_client.send(registration_id, options)
+     puts response
+     return response
+   else
+     puts "registration id is missing."
+   end
+  end
 end
