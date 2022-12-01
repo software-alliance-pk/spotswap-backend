94ddb0b295f32d2a5ef5101000c3cee24d64acc5
ror-vs <backend@viralsquare.org>
2022-12-01 22:31:10 +0500

    Add the cap files

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+set :application, "spot_swap"
+set :repo_url, "git@github.com:ViralSquare/spot_swap.git"
+
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
