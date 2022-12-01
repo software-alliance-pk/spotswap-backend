3532be6253421bf66be79e6653b12d65ef42e57e
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-12-01 21:32:04 +0500

    completed guidelines module

--- app/controllers/admins/guidelines_controller.rb
+++ app/controllers/admins/guidelines_controller.rb
@@ -1,5 +1,6 @@
 class Admins::GuidelinesController < ApplicationController
 	before_action :authenticate_admin!
+  before_action :find_faq, only: [:edit_faq, :destroy_faq, :update_faq]
 
 	def terms_and_conditions
     @terms_and_conditions = Page.where(title: "Terms & Conditions")
@@ -10,7 +11,7 @@ class Admins::GuidelinesController < ApplicationController
 	end
 
   def update_terms_and_conditions
-    @page = Page.find_by(id: params[:id])
+    @page = Page.find_by(id: params[:term_id])
     if @page.content.update(body: params[:description])
       redirect_to terms_and_conditions_admins_guidelines_path
       flash[:alert] = "Terms and Conditions have been updated successfully."
@@ -21,18 +22,59 @@ class Admins::GuidelinesController < ApplicationController
 	end
   
   def privacy_policy
+    @privacy_policy = Page.where(title: "Privacy Policy")
+	end
+
+  def edit_privacy_policy
+    @privacy_policy = Page.where(title: "Privacy Policy")
+	end
+
+  def update_privacy_policy
+    @page = Page.find_by(id: params[:policy_id])
+    if @page.content.update(body: params[:description])
+      redirect_to privacy_policy_admins_guidelines_path
+      flash[:alert] = "Privacy Policy has been updated successfully."
+    else
+      redirect_to privacy_policy_admins_guidelines_path
+      flash[:alert] = @page.content.errors.full_messages.to_sentence
+    end
 	end
 
   def faqs
-    @faqs = Page.where(title: "FAQ")
+    @faqs = Faq.all.order(created_at: :desc)
     @count = 0
 	end
 
-  def edit_faq
+  def add_faq
 	end
 
+  def create_faq
+    @faq = Faq.new(faq_params)
+    if @faq.save
+      redirect_to faqs_admins_guidelines_path
+      flash[:alert] = "FAQ has been created successfully."
+    else
+      redirect_to faqs_admins_guidelines_path
+      flash[:alert] = @faq.errors.full_messages.to_sentence
+    end
+	end
+
+  def edit_faq
+    @faq = Faq.find_by_id(params[:faq_id])
+	end
+
+  def update_faq
+    if @faq.update(faq_params)
+      redirect_to faqs_admins_guidelines_path
+      flash[:alert] = "FAQ has been updated successfully."
+    else
+      redirect_to faqs_admins_guidelines_path
+      flash[:alert] = @faq.errors.full_messages.to_sentence
+    end
+  end
+
   def destroy_faq
-    @faq = Page.find_by_id(params[:id])
+    @faq = Faq.find_by_id(params[:faq_id])
     if @faq.present?
       if @faq.destroy
         redirect_to faqs_admins_guidelines_path
@@ -53,4 +95,16 @@ class Admins::GuidelinesController < ApplicationController
       redirect_to new_admin_session_path, :notice => 'You need to sign in or sign up before continuing.'
     end
 	end
+
+  def find_faq
+    @faq = Faq.find_by_id(params[:faq_id])
+    unless @faq.present?
+      redirect_to faqs_admins_guidelines_path
+      flash[:alert] = "FAQ with this Id is not present."
+    end
+  end
+
+  def faq_params
+    params.permit(:question, :answer)
+  end
 end
