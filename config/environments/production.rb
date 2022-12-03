499d985db99d84400175ca1fb92e44c15667ad8a
ror-vs <backend@viralsquare.org>
2022-12-03 15:43:36 +0500

    Change the production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.css_compressor = nil
+  # config.assets.css_compressor = nil
   config.serve_static_assets = true
   config.assets.compile = true
-  config.assets.compile = false
   config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
