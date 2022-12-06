1719d3a3afd8ce0273186481dd2579bdfb116e6d
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-12-06 14:30:26 +0500

    updated user status channel

--- app/channels/user_status_channel.rb
+++ app/channels/user_status_channel.rb
@@ -4,31 +4,20 @@ class UserStatusChannel < ApplicationCable::Channel
     puts params
     if params[:user_id].present?
       stream_from "user_status_#{(params[:user_id])}"
-      puts params
       @conversation = Conversation.find_by(id: params[:conversation_id])
-      puts @conversation.present?
-      puts @conversation.user_id
-      puts @conversation.recepient_id
-      
       if @conversation.present?
-        puts "1111111111111111"
         if @conversation.user_id == params[:user_id].to_i
-          puts "22222"
           @user = @conversation.recepient
         elsif @conversation.recepient_id == params[:user_id].to_i
-          puts "33333333333"
           @user = @conversation.sender
-          puts @user
         end
         if @user.present?
-          puts "444444444444"
           stream_from("conversation_#{(params[:conversation_id])}")
-          payload = ActionCable.server.broadcast "user_status_#{@user.id}",
+          ActionCable.server.broadcast "user_status_#{@user.id}",
           {
             user_id: @user.id,
             user_online_status: @user.is_online,
           }
-          puts payload
         end
       else
         puts "conversation with this id is not present."
@@ -37,8 +26,4 @@ class UserStatusChannel < ApplicationCable::Channel
       puts "User Id is missing."
     end
   end
-
-  def unsubscribed
-    stop_all_streams
-  end 
 end
