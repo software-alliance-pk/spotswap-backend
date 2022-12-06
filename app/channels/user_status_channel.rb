fcd7905f948e0a402f3897d5945acfe656c7817c
ror-vs <backend@viralsquare.org>
2022-12-06 15:32:53 +0500

    Add the online status in json response

--- app/channels/user_status_channel.rb
+++ app/channels/user_status_channel.rb
@@ -1,29 +1,9 @@
 class UserStatusChannel < ApplicationCable::Channel
-
   def subscribed
-    puts params
-    if params[:user_id].present?
-      stream_from "user_status_#{(params[:user_id])}"
-      @conversation = Conversation.find_by(id: params[:conversation_id])
-      if @conversation.present?
-        if @conversation.user_id == params[:user_id].to_i
-          @user = @conversation.recepient
-        elsif @conversation.recepient_id == params[:user_id].to_i
-          @user = @conversation.sender
-        end
-        if @user.present?
-          stream_from("conversation_#{(params[:conversation_id])}")
-          ActionCable.server.broadcast "user_status_#{@user.id}",
-          {
-            user_id: @user.id,
-            user_online_status: @user.is_online,
-          }
-        end
-      else
-        puts "conversation with this id is not present."
-      end
-    else
-      puts "User Id is missing."
-    end
+    stream_from "user_status_#{current_user.id}"
+  end
+
+  def unsubscribed
+    stop_stream_from "user_status_#{current_user.id}"
   end
 end
--- app/views/api/v1/messages/create_conversation.json.jbuilder
+++ app/views/api/v1/messages/create_conversation.json.jbuilder
@@ -1,3 +1,6 @@
 json.conversation do
   json.partial! 'shared/conversation_details', conversation: @conversation
 end
+json.user_type @user_type
+json.sender_online_status @conversation.sender.is_online
+json.recepient_online_status @conversation.recepient.is_online
