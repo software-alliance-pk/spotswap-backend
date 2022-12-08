c42885c4a88e7df5c6f72dab88978bed5c2428a5
ror-vs <backend@viralsquare.org>
2022-12-08 19:28:37 +0500

    Add the Pay Pal payouts

---
+++ app/services/pay_pal_pay_outs_service.rb
@@ -0,0 +1,49 @@
+class PayPalPayOutsService < BaseService
+
+  def initialize
+    super
+  end
+
+  def  create_payout
+    uri = URI.parse("https://api-m.sandbox.paypal.com/v1/payments/payouts")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request["Authorization"] = "Bearer #{@token}"
+    request.body = JSON.dump({
+                               "sender_batch_header" => {
+                                 "sender_batch_id" => "PAYOUTS_#{create_batch_id}",
+                                 "recipient_type" => "EMAIL",
+                                 "email_subject" => "You have money!",
+                                 "email_message" => "You received a payment. Thanks for using our service!"
+                               },
+                               "payment_options" => {
+                                 "allowed_payment_method" => "INSTANT_FUNDING_SOURCE"
+                               },
+                               "items" => [
+                                 {
+                                   "amount" => {
+                                     "value" => "9.87",
+                                     "currency" => "USD"
+                                   },
+                                   "sender_item_id" => "201403140002",
+                                   "recipient_wallet" => "PAYPAL",
+                                   "receiver" => "sb-pyv7422138976@personal.example.com"
+                                 }
+                               ]
+                             })
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+  end
+
+
+  def create_batch_id
+    SecureRandom.hex(10)
+  end
+
+end
