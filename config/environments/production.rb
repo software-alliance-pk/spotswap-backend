66547332e1d742b937aaaf8544774a0d546a0f7c
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-12-14 18:16:58 +0500

    resolving get all slots api issue on aws during testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :debug
+  config.log_level = :info
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
