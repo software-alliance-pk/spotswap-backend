de760b5384af62be6f3ce47437c7453fca02478c
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-12-19 22:18:14 +0500

    worked on admin panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,7 +54,7 @@ class User < ApplicationRecord
   before_save :referral_code_generator
   after_commit :user_referral_code_record_generator
 
-  self.per_page = 10
+  self.per_page = 1
 
   private
 
