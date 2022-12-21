922a375d2a96dc23e3b64e1092e8153198dd6e23
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-12-21 12:59:41 +0500

    testing admin panel aws

--- app/models/car_model.rb
+++ app/models/car_model.rb
@@ -11,7 +11,7 @@ class CarModel < ApplicationRecord
 
   belongs_to :car_brand
   has_one_attached :image, dependent: :destroy
-  self.per_page = 2
+  self.per_page = 10
 
   validates :title, :color, :length, :width, :height, :released, presence: true
 end
