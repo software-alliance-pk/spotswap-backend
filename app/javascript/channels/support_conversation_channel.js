2ccb81083d8712a0fe277b742e406ccf99de5c90
ror-vs <backend@viralsquare.org>
2022-12-23 16:37:33 +0500

    Append message from the support

--- app/javascript/channels/support_conversation_channel.js
+++ app/javascript/channels/support_conversation_channel.js
@@ -1,5 +1,6 @@
 import consumer from "./consumer"
-$(document).ready(function() {
+
+$(document).ready(function () {
     const chat_id = $("#chatmessagebox").attr('data-id');
     const email = $("#admin_email").data("admin");
     console.log(chat_id);
@@ -14,56 +15,56 @@ $(document).ready(function() {
         },
 
         received(data) {
-            if(data.support_conversation_id== chat_id)
-            {
-                if(data.user_id == null)
-                {
-                    $(".msg_contain").append('<div class="msg admin">'+
-                        '<div class="profile">'+
-                        "<img src='/assets/default-profile.jpg'>"+
-                        '</div>'+
-                        '<div class="msg_wrapper">'+
-                        '<div class="msg_blk">'+
-                        '<p>'+data.body+'</p>'+
-                        '</div>'+
-                        '<p>'+data.created_at +'</p>'+
+            if (data.support_conversation_id == chat_id) {
+                if (data.user_id == null) {
+                    $(".msg_contain").append('<div class="msg admin">' +
+                        '<div class="profile">' +
+                        "<img src='/assets/default-profile.jpg'>" +
+                        '</div>' +
+                        '<div class="msg_wrapper">' +
+                        '<div class="msg_blk">' +
+                        '<p>' + data.body +
+                        '<div class="attch_blk">' +
+                        '<div class="img">' +
+                        "<img src=" + data.message_image + ">" +
+                        '</div>' +
+                        '<button type="button" class="btn down_btn">' +
+                        '<img src="/assets/icon-download.svg" alt="">' +
+                        '</button>' +
+                        '</div>' +
+                        '</p>' +
+                        '</div>' +
+                        '<p>' + data.created_at + '</p>' +
+                        '</div>' +
+                        '</div>');
+
+
+                } else {
+
+                    $(".msg_contain").append('<div class="msg user">' +
+                        '<div class="profile">' +
+                        (data.sender_image == null ? "<img src=" + data.sender_image + ">" : "<img src='/assets/default-profile.jpg'>") +
+                        '</div>' +
+                        '<div class="msg_wrapper">' +
+                        '<div class="msg_blk">' +
+                        '<p>Open a Support Ticket <strong>' +
+                        data.ticket_number.ticket_number +
+                        '</strong></p>' +
+                        '<p>' + data.body + '</p>' +
                         (data.message_image != null ?
-                        '<div class="attch_blk">'+
-                             "<img src="+data.message_image+">"+
-                        '</div>'
-                            : '')+
-                        '</div>'
-
-                    );
-
-
-                }
-                else
-                {
-
-                    $(".msg_contain").append('<div class="msg user">'+
-                        '<div class="profile">'+
-                        (data.sender_image == null ? "<img src="+data.sender_image+">" : "<img src='/assets/default-profile.jpg'>")+
-                        '</div>'+
-                        '<div class="msg_wrapper">'+
-                        '<div class="msg_blk">'+
-                        '<p>Open a Support Ticket <strong>'+
-                        data.ticket_number.ticket_number+
-                        '</strong></p>'+
-                        '<p>'+data.body+'</p>'+
-                        (data.message_image != null ?
-                            '<div class="attch_blk">'+
-                                "<img src="+data.message_image+">"+
-                                '<div class="img">'+'<img src="assets/icon-download.svg" alt="">'+'</div>'+
-                                    '<button type="button" class="btn down_btn">'+ '</button>'+
-                                '</div>': '')+
-                        '</div>'+'<p>'+data.created_at+'</p>'+
-                        '</div>'+
+                            '<div class="attch_blk">' +
+                            "<img src=" + data.message_image + ">" +
+                            '<div class="img">' + '<img src="/assets/icon-download.svg" alt="">' + '</div>' +
+                            '<button type="button" class="btn down_btn">' + '</button>' +
+                            '</div>' : '') +
+                        '</div>' + '<p>' + data.created_at + '</p>' +
+                        '</div>' +
                         '</div>');
 
 
                 }
                 $(".msg_contain").scrollTop($(".msg_contain")[0].scrollHeight);
+                $("#input_field_text").val('');
 
             }
         }
