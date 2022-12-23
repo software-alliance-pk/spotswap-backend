aa279a2de5fb75a202556375f59c0a625d553bc3
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2022-12-23 22:05:04 +0500

    worked on supports module

--- app/javascript/channels/support_conversation_channel.js
+++ app/javascript/channels/support_conversation_channel.js
@@ -4,7 +4,7 @@ let chat_id = ''
 $(document).on('turbolinks:load', function(){
     chat_id = $("#chatmessagebox").attr('data-id');
 });
-$(document).ready(function () {
+$(document).on('turbolinks:load', function(){
     chat_id = $("#chatmessagebox").attr('data-id');
     const email = $("#admin_email").data("admin");
     console.log(chat_id);
@@ -23,19 +23,18 @@ $(document).ready(function () {
                 if (data.user_id == null) {
                     $(".msg_contain").append('<div class="msg admin">' +
                         '<div class="profile">' +
-                        "<img src='/assets/default-profile.jpg'>" +
+                        (data.sender_image !== null ? "<img src=" + data.sender_image + ">" : "<img src='/assets/default-profile.jpg'>") +
                         '</div>' +
                         '<div class="msg_wrapper">' +
                         '<div class="msg_blk">' +
                         '<p>' + data.body +
                         '<div class="attch_blk">' +
                         '<div class="img">' +
-                        "<img src=" + data.message_image + ">" +
-                        '</div>' +
+                        (data.message_image !== null ? "<img src=" + data.message_image + ">" + '</div>' +
                         '<button type="button" class="btn down_btn">' +
                         '<img src="/assets/icon-download.svg" alt="">' +
                         '</button>' +
-                        '</div>' +
+                        '</div>' : "") +
                         '</p>' +
                         '</div>' +
                         '<p>' + data.created_at + '</p>' +
@@ -44,10 +43,9 @@ $(document).ready(function () {
 
 
                 } else {
-
                     $(".msg_contain").append('<div class="msg user">' +
                         '<div class="profile">' +
-                        (data.sender_image == null ? "<img src=" + data.sender_image + ">" : "<img src='/assets/default-profile.jpg'>") +
+                        (data.sender_image !== null ? "<img src=" + data.sender_image + ">" : "<img src='/assets/default-profile.jpg'>") +
                         '</div>' +
                         '<div class="msg_wrapper">' +
                         '<div class="msg_blk">' +
@@ -55,7 +53,7 @@ $(document).ready(function () {
                         data.ticket_number.ticket_number +
                         '</strong></p>' +
                         '<p>' + data.body + '</p>' +
-                        (data.message_image != null ?
+                        (data.message_image !== null ?
                             '<div class="attch_blk">' +
                             "<img src=" + data.message_image + ">" +
                             '<div class="img">' + '<img src="/assets/icon-download.svg" alt="">' + '</div>' +
@@ -64,8 +62,6 @@ $(document).ready(function () {
                         '</div>' + '<p>' + data.created_at + '</p>' +
                         '</div>' +
                         '</div>');
-
-
                 }
                 $(".msg_contain").scrollTop($(".msg_contain")[0].scrollHeight);
                 $("#input_field_text").val('');
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_16_184439) do
+ActiveRecord::Schema.define(version: 2022_12_23_102346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -262,7 +262,7 @@ ActiveRecord::Schema.define(version: 2022_12_16_184439) do
     t.boolean "read_status", default: false
     t.integer "sender_id"
     t.string "type"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["support_conversation_id"], name: "index_support_messages_on_support_conversation_id"
     t.index ["user_id"], name: "index_support_messages_on_user_id"
   end
