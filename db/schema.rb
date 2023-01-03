cde48f7d7321b0ce0571e946e8c40f2b55261e04
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-03 13:12:10 +0500

    add_email_to_paypal_account

--- db/schema.rb
+++ db/schema.rb
@@ -207,14 +207,14 @@ ActiveRecord::Schema.define(version: 2023_01_02_093717) do
     t.index ["user_id"], name: "index_parking_slots_on_user_id"
   end
 
-  create_table "paypal_partner_account", force: :cascade do |t|
+  create_table "paypal_partner_accounts", force: :cascade do |t|
     t.string "account_id"
     t.string "account_type"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
-    t.index ["user_id"], name: "index_paypal_partner_account_on_user_id"
+    t.index ["user_id"], name: "index_paypal_partner_accounts_on_user_id"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
@@ -373,7 +373,7 @@ ActiveRecord::Schema.define(version: 2023_01_02_093717) do
   add_foreign_key "messages", "users"
   add_foreign_key "mobile_devices", "users"
   add_foreign_key "parking_slots", "users"
-  add_foreign_key "paypal_partner_account", "users"
+  add_foreign_key "paypal_partner_accounts", "users"
   add_foreign_key "quick_chats", "users"
   add_foreign_key "stripe_connect_accounts", "users"
   add_foreign_key "support_conversations", "supports"
