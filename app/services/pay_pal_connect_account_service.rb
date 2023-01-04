10cffbd486b5d5a9b2d74136584d7c235144ea16
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-04 18:52:12 +0500

    working on paypal

--- app/services/pay_pal_connect_account_service.rb
+++ app/services/pay_pal_connect_account_service.rb
@@ -69,12 +69,12 @@ class PayPalConnectAccountService < BaseService
     if response.code  == "201"
       result = JSON.parse(response.body)
       if result["links"].present?
-        pay_pal_connect_id = result["links"].first["href"].split("/").last
-        account_type = "partner-referrals"
-        account = current_user.build_paypal_partner_account(account_id: pay_pal_connect_id, account_type: account_type, email: email)
-        account.save
+        # pay_pal_connect_id = result["links"].first["href"].split("/").last
+        # account_type = "partner-referrals"
+        # account = current_user.build_paypal_partner_account(account_id: pay_pal_connect_id, account_type: account_type, email: email)
+        # account.save
       end
-      return account_details = {account_id: result["links"].first["href"].split("/").last, response: result, account: account}
+      return account_details = { response: result}
     end
   end
 end
