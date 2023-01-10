6f66ac4869ce94ac946c9a6063d763d0bba96b99
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-10 14:35:02 +0500

    seed changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_05_113115) do
+ActiveRecord::Schema.define(version: 2023_01_05_145121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2023_01_05_113115) do
     t.string "contact"
     t.string "location"
     t.integer "status", default: 0
+    t.string "otp"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
