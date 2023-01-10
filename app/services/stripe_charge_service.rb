e00e797f50d6e4ac0c77a466ef90a3acca7b9106
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-10 22:49:17 +0500

    payment

--- app/services/stripe_charge_service.rb
+++ app/services/stripe_charge_service.rb
@@ -1,11 +1,11 @@
 class StripeChargeService
   Stripe.api_key ="sk_test_51MCjGDF5sdpBo10rWKvpkwEhZJbh48Ag1IFb9mFDt7JROqylvQX1M5z1cnP3toNkOgYwGNSAXeYziixrF5nhTIPW00JCq17CG3"
 
-  def charge_amount_from_customer(amount, account_id)
+  def charge_amount_from_customer(amount, customer_id)
     response = Stripe::Charge.create({
       amount: amount,
       currency: 'usd',
-      source: account_id,
+      customer: customer_id,
       description: 'My First Test Charge (created for API docs at https://www.stripe.com/docs/api)',
     })
     return response
