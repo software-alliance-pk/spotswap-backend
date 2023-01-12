269b6a3904a10653d007576996b4a14d7358333f
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-12 20:03:33 +0500

    added type in message broadcast

--- app/channels/user_status_channel.rb
+++
@@ -1,9 +0,0 @@
-class UserStatusChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "user_appearance_status"
-  end
-
-  def unsubscribed
-    stop_stream_from "user_appearance_status"
-  end
-end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,8 +2,4 @@ class Wallet < ApplicationRecord
   attr_accessor :wallet_amount
   belongs_to :user
   enum payment_type: [:credit_card, :paypal, :wallet]
-
-  #after_update :create_wallet_history
-
-  
 end
