b5b0c0606bc0e1bea8c23d72c0dcaf5fc71a1df2
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-12 23:03:28 +0500

    referral code fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_05_145121) do
+ActiveRecord::Schema.define(version: 2023_01_12_145922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -345,6 +345,7 @@ ActiveRecord::Schema.define(version: 2023_01_05_145121) do
     t.string "referral_code"
     t.string "stripe_connect_id"
     t.boolean "is_online", default: false
+    t.integer "referrer_id"
   end
 
   create_table "wallet_histories", force: :cascade do |t|
