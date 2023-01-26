ec3ff14965fa8387ce2ec5ff177b6c96d45bed89
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-27 00:11:40 +0500

    working on tayyab bhai issue

--- app/models/support_message.rb
+++ app/models/support_message.rb
@@ -2,7 +2,7 @@ class SupportMessage < ApplicationRecord
   belongs_to :support_conversation
   has_one_attached :image, dependent: :destroy
   has_one_attached :file, dependent: :destroy
-  belongs_to :sender, class_name: "User",foreign_key: :sender_id
+  belongs_to :sender, class_name: "User",foreign_key: :sender_id, optional: true
 
   after_create_commit { SupportMessageBroadcastJob.perform_later(self) }
 
