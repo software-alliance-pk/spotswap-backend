a2d0664182a861abf3f2661265646695c17f619c
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-01-31 16:40:20 +0500

    modified supports time

--- app/models/support_message.rb
+++ app/models/support_message.rb
@@ -5,5 +5,4 @@ class SupportMessage < ApplicationRecord
   belongs_to :sender, class_name: "User",foreign_key: :sender_id, optional: true
 
   after_create_commit { SupportMessageBroadcastJob.perform_later(self) }
-
 end
