e70de49232aaa57ad3f1ecdee3b652a3e9bd9e31
Abdullah Hafeez <abdullahhafeez1003@gmail.com>
2023-02-08 23:40:44 +0500

    fixing trello issues day2

--- app/controllers/admins/passwords_controller.rb
+++ app/controllers/admins/passwords_controller.rb
@@ -1,13 +1,12 @@
 class Admins::PasswordsController < Devise::PasswordsController
   
   def create
-    if Admin.admin.last.email == params[:admin][:email]
-      $otp = 6.times.map{rand(10)}.join
-      super
-    elsif
-      params[:admin][:email]==''
+    if params[:admin][:email]==''
       redirect_to new_admin_password_path
       flash[:alert] = "Email can't be blank"
+    elsif Admin.pluck(:email).include?(params[:admin][:email])
+      $otp = 6.times.map{rand(10)}.join
+      super
     else
       redirect_to new_admin_password_path
       flash[:alert] = "Invalid Email"
