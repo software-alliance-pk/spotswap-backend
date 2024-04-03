92952a9e7f48885fa2ec5d99a8b1332f38bc64bc
Ubuntu <ubuntu@ip-172-31-31-11.ec2.internal>
2024-04-03 08:25:10 +0000

    changes

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,6 @@ Bundler.require(*Rails.groups)
 
 module SpotSwap
   class Application < Rails::Application
-<<<<<<< HEAD
-    # ...
-
-=======
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
@@ -25,7 +21,6 @@ module SpotSwap
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # comment added
->>>>>>> f0661b8cf43d4ae4348048f77cd9b235be9a4637
     config.action_cable.disable_request_forgery_protection = true
     config.action_cable.url = "/cable"
 
