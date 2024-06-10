7188b57b3527c136e4c5a3682366ccc0915ee4ef
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-06-10 18:08:06 +0500

    changes

--- app/services/stripe_transfer_service.rb
+++ app/services/stripe_transfer_service.rb
@@ -16,21 +16,21 @@ class StripeTransferService
   def transfer_amount_to_owmer_and_customer(amount, account_id, conection_details )
     application_fee_amount = (amount * 0.30).to_i
     puts "Connection Details  === #{conection_details}"
-  #   paymentIntent = Stripe::PaymentIntent.create({
-  #   amount: 1099,
-  #   currency: 'eur',
-  #   customer: customer['id'],
-  #   # In the latest version of the API, specifying the `automatic_payment_methods` parameter
-  #   # is optional because Stripe enables its functionality by default.
-  #   automatic_payment_methods: {
-  #     enabled: true,
-  #   },
-  #   application_fee_amount: application_fee_amount,
-  #   transfer_data: {
-  #     destination: account_id,
-  #   },
-  # })
-  #   paymentIntent
+    paymentIntent = Stripe::PaymentIntent.create({
+    amount: amount,
+    currency: 'usd',
+    # customer: customer['id'],
+    # In the latest version of the API, specifying the `automatic_payment_methods` parameter
+    # is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+    application_fee_amount: application_fee_amount,
+    transfer_data: {
+      destination: account_id,
+    },
+  })
+    paymentIntent
 
   end
 
