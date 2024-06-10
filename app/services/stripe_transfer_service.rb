0b86f4b120fef1e018cbcb14ff05b08ff7de8a06
Haseeb71 <71abdulhaseeb71@gmail.com>
2024-06-10 16:05:23 +0500

    chanegs

--- app/services/stripe_transfer_service.rb
+++ app/services/stripe_transfer_service.rb
@@ -3,22 +3,13 @@ class StripeTransferService
   Stripe.api_key = ENV['STRIPE_API_KEY']
   Stripe.api_key = ENV['STRIPE_API_KEY'] if Rails.env.production?
   
-  def transfer_amount_of_top_up_to_customer_connect_account(amount, recipient_account_id)
-    application_fee_amount = (amount * 0.30).to_i
-
-    payment_intent = Stripe::PaymentIntent.create({
+  def transfer_amount_of_top_up_to_customer_connect_account(amount, account_id)
+    response = Stripe::Transfer.create({
       amount: amount,
       currency: 'usd',
-      payment_method_types: ['card'],
-      application_fee_amount: application_fee_amount,
-      transfer_data: {
-        destination: recipient_account_id
-      }
-    }, {
-      stripe_account: recipient_account_id
+      destination: account_id
     })
-
-    payment_intent
+    return response
   end
 
   def connect_balance_check(account_id)
