f6e802dc563dc15ab05fbd432cec9f7d01cebdc7
Aqsa <aqsajabbar7689@gmail.com>
2024-08-19 18:19:38 +0500

    support chat authorization issue resolved

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,10 +4,38 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      begin
+        if request.headers['Authorization'].present?
+          header = request.headers['Authorization']
+          header = header.split(' ').last if header
+          begin
+            @decoded = JsonWebToken.decode(header)
+            @current_user = User.find_by_id(@decoded[:user_id]) || User.find_by_email(@decoded[:email])
+          rescue ActiveRecord::RecordNotFound => e
+            logger.info "Record Not Found: #{e.message}"
+            reject_unauthorized_connection
+          rescue JWT::DecodeError => e
+            logger.info "JWT Decode Error: #{e.message}"
+            reject_unauthorized_connection
+          end
+        else
+          self.current_user = find_verified_admin
+        end
+      rescue
+        puts "connection is not created"
+      end
     end
 
     private
+
+    def find_verified_admin
+      if verified_user = env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
+
       def find_verified_user
         if verified_user = User.find_by(id: cookies.encrypted[:user_id])
           verified_user
